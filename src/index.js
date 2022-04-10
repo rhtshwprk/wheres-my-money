4cc62229168292979d2fede951050bf50cc512eb
Rohitashwa Pareek <rohitashwapareek@gmail.com>
2022-04-10 20:45:10 +0530

    Added Auth0 authentication

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-// import { Auth0Provider } from "@auth0/auth0-react";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  // <Auth0Provider
-  //   domain="hacknyu.jp.auth0.com"
-  //   clientId="alqlsr2XUTxw7cjdbjJiIhl40MVm6EGo"
-  //   redirectUri="http://localhost:3000/"
-  // >
-  <App />,
-  // </Auth0Provider>,
+  <Auth0Provider
+    domain="hacknyu.jp.auth0.com"
+    clientId="v1XCHBsdcBoVR2cjS4VRiuDmj28VwkVy"
+    redirectUri="http://localhost:3000/"
+  >
+    <App />,
+  </Auth0Provider>,
   document.getElementById("root")
 );
